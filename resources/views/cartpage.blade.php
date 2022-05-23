49c57de2444696383cda64676d9a160fedd0dade
salar motevalli <salar.mo.ro@gmail.com>
2022-05-23 23:09:36 +0430

    make ui for cart page

--- resources/views/cartpage.blade.php
+++ resources/views/cartpage.blade.php
@@ -1,1 +1,58 @@
-<h1>salam salar</h1>
+<div class="container mt-5">
+    <div class="d-lg-flex p-3 bg-white justify-content-lg-between shadow rounded-4 text-start">
+        
+        <section class="p-2 rounded-4">
+            <table class="table table-striped w-100">
+                <thead>
+                    <tr>
+                        <td>row</td>
+                        <td>image</td>
+                        <td>name</td>
+                        <td>price</td>
+                        <td>quantity</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">1</td>
+                        <td>
+                            <img class="rounded-3" src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg" width="150px">
+                        </td>
+                        <td>banana pen</td>
+                        <td>$1000</td>
+                        <td>5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    
+        <section class="p-3 bg-info rounded-4">
+            
+            <div class="text-white mb-4">
+                <small class="text-secondary h5 text-md">total:</small>
+                <div class="border-bottom"></div>
+                <p class="text-white h3 ml-4 mt-1">$1000</p>
+            </div>
+            
+            <div class="text-white mb-5">
+                <small class="text-secondary h5 text-md">total:</small>
+                <div class="border-bottom"></div>
+                <p class="text-white h3 ml-4 mt-1">$1000</p>
+            </div>
+
+            <div class="text-white mb-5">
+                <small class="text-secondary h5 text-md">total:</small>
+                <div class="border-bottom"></div>
+                <p class="text-white h3 ml-4 mt-1">$1000</p>
+            </div>
+
+            <div>
+                <button class="btn btn-secondary">
+                    pay order
+                </button>
+            </div>
+        </section>
+        
+    </div>
+</div>
+
