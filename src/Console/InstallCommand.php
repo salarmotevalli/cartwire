f6e4bb896946b4eb5f1ec695a10615fe51f66d23
salarmotevalli <salar.mo.ro@gmail.com>
2022-12-01 22:51:07 +0330

    add some stubs and console command for installation

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command 
+{
+    protected $signiture = 'cartwire:install {ui}';
+
+    protected $description = 'Install the Cartwire components and resources';
+
+    public function handle()
+    {
+        
+    }
+    
+}
