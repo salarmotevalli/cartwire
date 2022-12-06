71d389d1eca058ca8610966c79b4f6db92f15909
salarmotevalli <salar.mo.ro@gmail.com>
2022-12-06 13:33:19 +0330

    add trait for enum

--- src/Contracts/Enums/TableColumnStatus.php
+++ src/Contracts/Enums/TableColumnStatus.php
@@ -2,11 +2,13 @@
 
 namespace Salarmotevalli\CartWire\Contracts\Enums;
 
+use Salarmotevalli\CartWire\Contracts\Traits\EnumTrait;
 
 enum TableColumnStatus: int {
 
+    use EnumTrait;
 
     case NULLABLE = 0;
     case REQUIRED = 1;
-
+    
 }
